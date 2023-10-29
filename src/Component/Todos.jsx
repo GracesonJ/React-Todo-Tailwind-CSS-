2c527ac361c35306f820d758a180203d626ef632
Graceson j <gracesonjjohnson@gmail.com>
2023-10-30 00:25:40 +0530

    tailwindcss updated

--- src/Component/Todos.jsx
+++ src/Component/Todos.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+
 // import index.css
 function Todos() {
     const [tasks, setTasks]= useState([])
@@ -15,13 +16,14 @@ function Todos() {
       updateList.splice(index,1)
       setTasks(updateList)
     }
+
   return (
     <>
-      <div className='sm:p-5 md:py-1 md:px-1'>
-      <div className="flex flex-col items-center bg-blue-200 min-h-screen min-w-fitmin-w-fit">
-            <h1 className='text-5xl m-16 font-bold'>Todo List</h1>
+      <div className='bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500  min-w-fitmin-w-fit'>
+      <div className="flex flex-col items-center min-h-screen">
+            <h1 className='text-5xl m-16 font-bold text-black-600'>Todo List</h1>
             <div className="">
-                <input className='bg-slate-200 rounded-md p-3 m-3' 
+                <input className='bg-slate-200 rounded-md p-3 ml-10' 
                 type="text" 
                 placeholder="Add Todos"
                 value={task}
@@ -36,15 +38,16 @@ function Todos() {
                   <ul>
                     {
                       tasks.map((task, index)=>(
-                        <div className='flex bg-slate-100 m-4 py-4 pl-10 pr-4 rounded-md'>
-                          <li className='self-center font-semibold pr-10 mr-6 grow'>{task}</li>
-                          <button onClick={()=>deleteTasks(index)} className='bg-red-500 text-white p-2 mx-1 rounded-md font-bold hover:bg-red-700'>Delete</button>
+                        <div className='flex bg-slate-100 m-4 py-4 pl-10 pr-4 rounded-md w-full'>
+                          <li className='self-center font-semibold pr-8 mr-20 grow'>{task}</li>
+                          {/* <button className='bg-blue-500 text-white p-2 mx-4 rounded-md font-bold hover:bg-blue-700'><i class="fa-solid fa-pen"></i></button> */}
+                          <button onClick={()=>deleteTasks(index)} className='bg-red-500 text-white p-2 mx-1 rounded-md font-bold hover:bg-red-700'><i className="fa-solid fa-trash"></i></button>
                         </div>
                     ))}
                   </ul>
                 ):(
                   <div>
-                    <p className='text-xl m-16 font-bold'>No Task Found</p>
+                    <p className='text-xl m-16 font-bold text-black-100'>No Task Found</p>
                   </div>
                 )}
             </div>
