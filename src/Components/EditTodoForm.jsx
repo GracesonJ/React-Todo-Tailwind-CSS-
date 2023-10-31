ced49f2266ad410ab51fde0241ec1077664bae54
Graceson j <gracesonjjohnson@gmail.com>
2023-10-31 23:39:55 +0530

    code updated

--- src/Components/EditTodoForm.jsx
+++ src/Components/EditTodoForm.jsx
@@ -10,9 +10,9 @@ export const EditTodoForm = ({editTodo, task}) => {
         editTodo(value, task.id);
       };
   return (
-    <form onSubmit={handleSubmit} className="flex  bg-slate-100 m-4 py-3 px-5 rounded-md justify-between items-center w-10/12 ">
-    <input type="text" value={value} onChange={(e) => setValue(e.target.value)} className="outline-none bg-slate-100" placeholder='Update task' />
-    <button type="submit" className='text-white p-1 m-1 rounded-md font-bold hover:bg-green-600 bg-green-500'>Update Task</button>
+    <form onSubmit={handleSubmit} className="flex  bg-slate-100 m-2 py-1 px-5 rounded-md justify-center items-center w-10/12 ">
+    <input type="text" value={value} onChange={(e) => setValue(e.target.value)} className="outline-none bg-slate-100 flex-1 m-2 p-2" placeholder='Update task' />
+    <button type="submit" className='text-white p-2 m-2 rounded-md flex-none font-bold hover:bg-green-600 bg-green-500'>Update</button>
   </form>
   )
 }
--- src/Components/TodoForm.jsx
+++ src/Components/TodoForm.jsx
@@ -14,14 +14,14 @@ export const TodoForm = ({addTodo}) => {
         }
       };
   return (
-    <form onSubmit={handleSubmit} className="TodoForm w-full">
+    <form onSubmit={handleSubmit} className="TodoForm w-11/12 flex">
     <input 
       type="text" 
       value={value} 
       onChange={(e) => setValue(e.target.value)} 
-      className="todo-input bg-slate-200 rounded-md p-3 ml-10 w-3/5 outline-none" 
+      className=" flex-1 bg-slate-200 rounded-md m-3 p-3 ml-5 w-3/5 outline-none " 
       placeholder='Whats on your mind ?' />
-    <button type="submit" className='bg-green-500 text-white p-3 m-3 rounded-md font-bold hover:bg-green-600'>Add Task</button>
+    <button type="submit" className='bg-green-500 text-white flex-none p-3 m-3 rounded-md font-bold hover:bg-green-600'>Add Task</button>
   </form>
   )
 }
